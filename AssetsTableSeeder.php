293ba64ad6f73152bdeaa67f7aa2e0339b571ee1
Amirul Aron <amirul.aron@hotmail.my>
2019-03-08 02:51:23 +0000

    Seeder prepared

--- AssetsTableSeeder.php
+++ AssetsTableSeeder.php
@@ -11,6 +11,22 @@ class AssetsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $datetime = date('Y-m-d H:i:s');
+        DB::table('assets')->insert([
+            [
+                'name'       => NULL,
+		    	'type'       => NULL,
+				'address'    => NULL,
+				'location'   => NULL,
+				'city'       => NULL,
+				'postcode'   => NULL,
+				'state'      => NULL,
+				'country'    => NULL,
+				'latitude'   => NULL,
+				'longitude'  => NULL,
+                'created_at' => $datetime,
+                'updated_at' => $datetime,
+            ],     
+        ]);
     }
 }
